44c439f6a0a4fefe007e449be7112ad68ed6d82a
CodeStack <codestack.net@gmail.com>
2018-04-02 19:52:32 +1000

    Added vault browser utility

---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿//Developed by www.codestack.net
+//License: https://github.com/codestack-net-dev/pdm-vault-browser/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CodeStack.Dev.PdmVaultBrowser
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var path = "";
+
+            if (args.Any())
+            {
+                path = args.First();
+            }
+
+            var dlg = new OpenFileDialog()
+            {
+                InitialDirectory = path,
+                Multiselect = true
+            };
+
+            dlg.ShowDialog();
+        }
+    }
+}
